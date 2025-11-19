81c6d151a3939695aa89e46f4e34f6f7d493d84f
Marc Pieper <mar@pupil-labs.com>
2025-11-19 08:58:58 +0100

    Added a real-time visualization example

---
+++ examples/real-time_visualization.py
@@ -0,0 +1,20 @@
+import cv2
+
+from pupil_labs.neon_usb import EyeCamera, SceneCamera
+
+scene_cam = SceneCamera()
+eye_cam = EyeCamera()
+
+
+while True:
+    scene_frame = scene_cam.get_frame()
+    eye_frame = eye_cam.get_frame()
+
+    cv2.imshow("Neon Eye Camera", eye_frame.gray)
+    cv2.imshow("Neon Scene Camera", scene_frame.bgr)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
