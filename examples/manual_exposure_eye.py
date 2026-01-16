6a6d2cc6ec831cb95f4c5c269825a37cf2f0d24e
Dominic Canare <dom@dominiccanare.com>
2026-01-17 04:28:50 +1300

    Opt: use libuvc to eye camera exposure

--- examples/manual_exposure_eye.py
+++ examples/manual_exposure_eye.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 
 from pupil_labs.neon_usb.cameras.eye import EyeCamera
@@ -10,6 +12,7 @@ while True:
     frame = camera.get_frame()
     image = frame.bgr
     print("exposure:", camera.exposure)
-    camera.exposure = (counter % 200, counter % 400)
+    camera.exposure = (counter % 50, (counter * 2) % 50)
     cv2.imshow("Neon Sensor Module", image)
     cv2.waitKey(1)
+    time.sleep(0.05)
